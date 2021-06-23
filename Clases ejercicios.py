209642ce786db7d131e55aa3d1e39208f40d4690
goz310103 <tamiguisimo@gmail.com>
2021-06-22 23:55:21 -0600

    José A: Creé la clase empleado, le asigné sus atributos, métodos y determiné una función para averiguar el promedio mayor

--- Clases ejercicios.py
+++ Clases ejercicios.py
@@ -1,3 +1,67 @@
 """
 Ejercicios clases python
 """
+""""
+3. Se va a definir un objeto empleado que tenga los siguientes datos: numero de_
+empleado, nombre, salario por hora. El objeto va a contener los siguientes metodos:
+mostrar () que despliega los datos de una instancia
+calculosalario (horas) que recibe una cantidad de horas y muestra cual
+deberia ser el salario.
+*Implementar esta clase en Python.
+*Defina una lista de instancias de la clase empleado. Escriba una funcion que
+recorra esta lista y encuentro el empleado con salario mayor.
+"""
+
+
+class Empleado:
+    def __init__(self, numero,nombre,salario):
+        self.numero = numero
+        self.nombre = nombre
+        self.salario = salario
+        self.deuda = 100000
+    def mostrar(self):
+        print("Número de empleado: " + str(self.numero),"Nombre del empleado: " + str(self.nombre),"Salario del empleado: " + str(self.salario))
+
+    def calculo_salario(self,horas):
+        print("Su salario corresponde a", self.salario*horas, "si trabaja", horas, "horas")
+
+    def aumentar_deuda(self,monto):
+        self.deuda += monto
+        print("La deuda es de "+ str(self.deuda))
+
+    def reducir_deuda(self,monto):
+        self.deuda -= monto
+        if self.deuda == 0:
+            return "Felicidades, ya pagó su cuota"
+
+
+
+empleado1 = Empleado(12,"Juan", 10000)
+empleado2 = Empleado(343,"Esteban", 134000)
+empleado3 = Empleado(1,"Ramiro", 1021000)
+empleado4 = Empleado(1145,"Pedro", 12000)
+
+empleado1.aumentar_deuda(1000)
+lista_empleados = [empleado1,empleado2,empleado3,empleado4]
+
+def salario_mayor(empleados):
+    if not isinstance(empleados, list):
+        return "error"
+    mayor = 0
+    empleado_mayor = None
+    for empleado in empleados:
+        if mayor < empleado.salario:
+            mayor = empleado.salario
+            empleado_mayor = empleado
+        else:
+            pass
+    print("El empleado con mayor salario corresponde a: ")
+    return empleado_mayor.mostrar()
+
+salario_mayor(lista_empleados)
+
+
+
+
+
+
